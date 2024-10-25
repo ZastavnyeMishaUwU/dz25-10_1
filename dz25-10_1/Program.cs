22824c1e6bb3603389051a106db5ae2f693a2137
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-10-25 20:26:13 +0300

    Add project files.

---
+++ dz25-10_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace dz25_10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть ваше ім'я: ");
+            string name = Console.ReadLine();
+            Console.Write("Введіть час доби (у годинах): ");
+            int hour = int.Parse(Console.ReadLine());
+            string greeting;
+
+            if (hour < 12)
+            {
+                greeting = "Доброго ранку";
+            }
+            else if (hour < 18)
+            {
+                greeting = "Добрий день";
+            }
+            else
+            {
+                greeting = "Добрий вечір";
+            }
+            Console.WriteLine($"{greeting}, {name}!");
+        }
+    }
+}
